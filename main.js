13e8d0b072055e27a7c3cae666aad26579ae55fb
User-Takhirov <tohirovfayruz@gmail.com>
2024-08-19 16:55:40 +0500

    js day-14

---
+++ main.js
@@ -0,0 +1,66 @@
+// musbat sonlarning yigindisi
+// let arr = [12, 5, 3, -6, -2];
+// const x = (arr) => {
+//   let num = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//       num += arr[i];
+//     }
+//   }
+//   return num;
+// };
+// console.log(x(arr));
+
+//
+
+// function User() {
+//   this.name = "gofur";
+//   this.lastName = "Ergashev";
+//   this.age = 4;
+//   this.show = function () {
+//     console.log(this.name);
+//   };
+// }
+
+//123ni teskarisini chiqarish
+// let a = 123;
+// let b = a % 100;
+// let c = (a - b) / 100;
+// let d = (b - (a % 10)) / 10;
+// let e = b % 10;
+// let f = e * 100 + d * 10 + c;
+// console.log(f);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//123ni teskarisini chiqarish
+// let a = 123;
+// const x = (num) => {
+//   let result = "";
+//   for (let i = String(num).length - 1; i >= 0; i--) {
+//     result += String(num)[i];
+//   }
+//   return Number(result);
+// };
+// console.log(x(a));
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//priceni hisoblash
+// let arr = [
+//   { price: 10, id: 2 },
+//   { price: 5, id: 1 },
+//   { price: 15, id: 4 },
+//   { price: 20, id: 6 },
+//   { price: 50, id: 7 },
+// ];
+// const x = (arr) => {
+//   priceCount = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     priceCount += arr[i].price;
+//   }
+//   return priceCount;
+// };
+// console.log(x(arr));
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
